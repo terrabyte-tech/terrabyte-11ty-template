2d584a70c1d5bb3f28766c30e3920ed12b9ce51f
sharper <hi@sammyharper.com>
2025-06-13 17:37:30 -0500

    feat: init 11ty

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function (eleventyConfig) {
+  // copy site data
+  eleventyConfig.addPassthroughCopy('htaccess.txt');
+  eleventyConfig.addPassthroughCopy('.htaccess');
+  // copy directories to the output
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('js');
+  eleventyConfig.addPassthroughCopy('img');
+  // copy favicons
+  eleventyConfig.addPassthroughCopy('site.webmanifest');
+  eleventyConfig.addPassthroughCopy('*.ico');
+  eleventyConfig.addPassthroughCopy('*.png');
+
+  // watch directories for changes
+  eleventyConfig.addWatchTarget('css');
+  eleventyConfig.addWatchTarget('js');
+  eleventyConfig.addWatchTarget('img');
+  eleventyConfig.addWatchTarget('cards');
+  eleventyConfig.addWatchTarget('site.webmanifest');
+
+};
