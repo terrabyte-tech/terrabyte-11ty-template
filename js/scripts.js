4a42f800fdd9434bf5e0b26631577640f8a7172d
sharper <hi@sammyharper.com>
2025-06-17 23:50:47 -0500

    feat: add standardized code comments; splash design to head section

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function(){
 
   console.log("{{ site.title }} scripts.js loaded");
 
-///////////////
+  // Add a class to the body element to indicate that the page has loaded
+  document.body.classList.add("loaded");
   
 }, false);
