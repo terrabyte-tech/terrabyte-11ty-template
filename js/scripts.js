7bd2370aa2770884debb8b9fa6d7f063b742ce63
sharper <hi@sammyharper.com>
2025-06-20 13:58:13 -0500

    fix: minor additions of comments and planned inclusions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 window.addEventListener("load", function(){
 
-  console.log("{{ site.title }} scripts.js loaded");
+  // var thisScript = document.querySelector("script[src='./js/scripts.js']");
+  // var thisScriptSiteTitle = thisScript.getAttribute("data-site-title");
+
+  // console.log(thisScript);
+  console.log(thisScriptSiteTitle + " scripts.js loaded");
 
   // Add a class to the body element to indicate that the page has loaded
   document.body.classList.add("loaded");
