13d54969fbda892ddb94d5ad034d8fac30c79f57
sharper <hi@sammyharper.com>
2025-08-19 12:08:33 -0500

    feat: build sitemap in 11ty build process

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
 window.addEventListener("load", function(){
 
-  // var thisScript = document.querySelector("script[src='./js/scripts.js']");
-  // var thisScriptSiteTitle = thisScript.getAttribute("data-site-title");
-
-  // console.log(thisScript);
-  console.log("scripts.js loaded");
+  console.log(`[${window.siteData.project}] scripts.js loaded`);
 
   // Add a class to the body element to indicate that the page has loaded
   document.body.classList.add("loaded");
