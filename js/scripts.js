4b05784016b8b3a2eb6805cc0ac50392837b3730
sharper <hi@sammyharper.com>
2025-09-05 16:08:26 -0500

    feat: implement version sync script between template package and site json

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function(){
 
   console.log(`[${window.siteData.project}] scripts.js loaded`);
+  console.log(`Using template version [${window.siteData.templateVersion}].`);
 
   // Add a class to the body element to indicate that the page has loaded
   document.body.classList.add("loaded");
---
+++ scripts/sync-version.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// Paths to your files
+const packagePath = path.resolve(__dirname, '../package.json');
+const sitePath = path.resolve(__dirname, '../_data/site.json');
+
+// Read package.json
+const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+const version = pkg.version;
+console.log(`Package version: ${version}`);
+
+
+// Read site.json
+const site = JSON.parse(fs.readFileSync(sitePath, 'utf8'));
+
+// Update templateVersion
+site.templateVersion = version;
+
+// Write back to site.json
+fs.writeFileSync(sitePath, JSON.stringify(site, null, 2));
+
+console.log(`Changed templateVersion to ${version} in site.json`);
