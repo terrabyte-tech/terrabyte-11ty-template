5d49a7a64cc666d359382568823e670d35231334
sharper <hi@sammyharper.com>
2025-09-05 22:29:05 -0500

    feat: include .htaccess in site build process

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,12 @@ const site = require("./_data/site.json");
 
 module.exports = function (eleventyConfig) {
   // copy site data
-  // eleventyConfig.addPassthroughCopy('.htaccess');
+  eleventyConfig.addPassthroughCopy('.htaccess');
   // copy directories to the output
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('img');
   // copy favicons
-  // eleventyConfig.addPassthroughCopy('site.webmanifest');
   eleventyConfig.addPassthroughCopy('*.ico');
   eleventyConfig.addPassthroughCopy('*.png');
 
