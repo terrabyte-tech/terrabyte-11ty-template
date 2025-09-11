d7cf76b1abf46bc839421d38ae0c4186b6120cea
sharper <hi@sammyharper.com>
2025-09-10 23:51:16 -0500

    dev: update tagging in shell script to add annotation

--- scripts/release-patch.sh
+++ scripts/release-patch.sh
@@ -12,7 +12,8 @@ VERSION=$(node -p "require('./package.json').version")
 git commit -m "chore(release): v$VERSION - sync site.json with package version"
 
 # Tag the commit with the new version from package.json
-git tag "v$VERSION" -m "$VERSION"
+# git tag "v$VERSION" -m "$VERSION"
+git tag -a "v$VERSION" -m "$VERSION"
 
 echo "Version bumped to v$VERSION, site.json synced, git tag created."
 echo "Template ready to release!"
