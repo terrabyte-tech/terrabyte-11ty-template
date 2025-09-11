93f586079c4123c250c5b3f14b2fcce9106e11f2
sharper <hi@sammyharper.com>
2025-09-11 15:56:51 -0500

    feat: add shared pixel images/icons to template

--- js/site-scripts.js
+++ js/site-scripts.js
@@ -5,5 +5,7 @@ window.addEventListener("load", function(){
 
   // Add a class to the body element to indicate that the page has loaded
   document.body.classList.add("loaded");
+
+  // custom script here
   
 }, false);
